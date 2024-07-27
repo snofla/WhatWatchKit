6c6332ec718fac64fd472c7680dc047150760a09
Alfons Hoogervorst <alfons.hoogervorst@gmail.com>
2024-07-27 16:15:28 +0200

    initial commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "WhatWatchKit",
+    platforms: [.iOS(.v17), .macOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,9 +16,15 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "WhatWatchKit"),
+            name: "WhatWatchKit",
+            resources: [
+                .copy("Resources/WhatWatchModel.mlmodelc")
+            ]
+        ),
         .testTarget(
             name: "WhatWatchKitTests",
-            dependencies: ["WhatWatchKit"]),
+            dependencies: ["WhatWatchKit"],
+            resources: [.copy("Resources")]
+        ),
     ]
 )
--- Tests/WhatWatchKitTests/WhatWatchKitTests.swift
+++ Tests/WhatWatchKitTests/WhatWatchKitTests.swift
@@ -2,11 +2,65 @@ import XCTest
 @testable import WhatWatchKit
 
 final class WhatWatchKitTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    func test_Diver() async throws {
+        let path = self.imageURL(for: "diver_100")
+        XCTAssertNotNil(path)
+        let results = try await Recognize.categoryOfWatch(at: path!)
+        XCTAssert(results.count > 1)
+        XCTAssert(results.first!.label == .diver)
     }
+    
+    func test_Dress() async throws {
+        let path = self.imageURL(for: "dress_100")
+        XCTAssertNotNil(path)
+        let results = try await Recognize.categoryOfWatch(at: path!)
+        XCTAssert(results.count > 1)
+        XCTAssert(results.first!.label == .dress)
+    }
+
+    func test_Sport() async throws {
+        let path = self.imageURL(for: "sport_100")
+        XCTAssertNotNil(path)
+        let results = try await Recognize.categoryOfWatch(at: path!)
+        XCTAssert(results.count > 1)
+        XCTAssert(results.first!.label == .sport)
+    }
+
+    func test_Chronograph() async throws {
+        let path = self.imageURL(for: "chronograph_100")
+        XCTAssertNotNil(path)
+        let results = try await Recognize.categoryOfWatch(at: path!)
+        XCTAssert(results.count > 1)
+        XCTAssert(results.first!.label == .chronograph)
+    }
+
+    func test_Field() async throws {
+        let path = self.imageURL(for: "field_100")
+        XCTAssertNotNil(path)
+        let results = try await Recognize.categoryOfWatch(at: path!)
+        XCTAssert(results.count > 1)
+        XCTAssert(results.first!.label == .field)
+    }
+
+    func test_Pilot() async throws {
+        let path = self.imageURL(for: "pilot_100")
+        XCTAssertNotNil(path)
+        let results = try await Recognize.categoryOfWatch(at: path!)
+        XCTAssert(results.count > 1)
+        XCTAssert(results.first!.label == .pilot)
+    }
+
+    func test_GMT() async throws {
+        let path = self.imageURL(for: "gmt_100")
+        XCTAssertNotNil(path)
+        let results = try await Recognize.categoryOfWatch(at: path!)
+        XCTAssert(results.count > 1)
+        XCTAssert(results.first!.label == .gmt)
+    }
+
+    func imageURL(for name: String) -> URL? {
+        Bundle.module.url(forResource: name, withExtension: "png", subdirectory: "Resources")
+    }
+    
 }
